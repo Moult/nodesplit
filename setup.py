cd583597d2eb05fcdeef9e2274c68b9552697084
Dion Moult <dion@thinkmoult.com>
2014-03-08 18:29:04 +1100

    Basic project structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name = 'Nodesplit',
+      version = '1.0',
+      description = 'Contained 3D node splitting and growing script',
+      author = 'Dion Moult',
+      author_email = 'dion@thinkmoult.com',
+      url = 'http://thinkmoult.com',
+      packages = ['Nodesplit'],
+      package_dir = {'': 'src'},
+      )
